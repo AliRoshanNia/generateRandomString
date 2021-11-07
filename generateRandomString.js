62ead2f84eb42146335d7878add1a6e30e6f56fa
ali roshan nia <ali.roshan.nia.1379@gmail.com>
2021-11-07 22:09:25 +0330

    first commit

---
+++ generateRandomString.js
@@ -0,0 +1,39 @@
+export default class generateRandomeString {
+    english = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "u", "w", "x", "y", "z",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "U", "W", "X", "Y", "Z",
+        "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "}", "{", "~", "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+    ]
+    persian = ["ا", "ب", "پ", "ت", "ث", "ج", "چ", "ح", "خ", "د", "ذ", "ر", "ز", "ٓژ", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ", "ف", "ق", "ک", "گ", "ل", "م", "ن", "و", "ه", "ی", "/",
+        "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "}", "{", "~", "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+    ]
+    generator(len, lang) {
+        let result = '';
+        for (let i = 0; i < len; i++) {
+            let num = getRandome()
+            result += getCharachter(num, lang)
+        }
+
+        return result
+    }
+    getCharachter(num, lang) {
+        if (lang.toLowerCase() == 'persian') {
+            if (num < persian.length) {
+                return persian[num]
+            } else {
+                num = getRandome()
+                getCharachter(num)
+            }
+        } else
+        if (lang.toLowerCase() == 'english') {
+            if (num < english.length) {
+                return english[num]
+            } else {
+                num = getRandome()
+                getCharachter(num)
+            }
+        }
+    }
+    getRandome() {
+        return Math.floor(Math.random() * 30)
+    }
+}
